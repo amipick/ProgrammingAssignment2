2b7edf5e4c1d9a87d310694261600e2284a87979
amipick <ami.pick@yahoo.com>
2015-12-17 14:22:19 -0500

    Update cachematrix.R
    typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Below are two function that take a squere (2X2) matrix and returns its inverse values.
 
 ###### to test use: a <- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2)) #######
-# 1st function: makeCacheMatrix takes a given 2 X 2 mmatrix and also provides a list containig function to: 
+# 1st function: makeCacheMatrix takes a given 2 X 2 matrix and also provides a list containing function to: 
 # 1. set matrix 
 # 2. get matrix
 # 3. set matrix inverse
@@ -16,7 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
   }
   get <- function() x # returns the original matrix. test with a$get()
   setinverse <- function(solve) inv <<- solve # do not test!!
-  getinverse <- function() inv  # if tested before cashSolve function will return "null" else will return matrix inverse. test with a$getinverse() 
+  getinverse <- function() inv  # if tested before cashSolve function will return "null" else will return matrix inverse. 
+  # test with a$getinverse() 
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -33,5 +34,5 @@ cacheSolve <- function(x, ...) {
   data <- x$get()  # in case values are not in cache
   inv <- solve(data) # compute using solve()
   x$setinverse(inv)  # store in cache
-  inv                # return vmatrix inverse
+  inv                # return matrix inverse
 } 
